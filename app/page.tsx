0262e1385544d92b01c804f0481b2c1fc3eb4ebb
marco-soria <marco.soriaa7@gmail.com>
2024-08-10 17:01:05 -0500

    primitive types

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,24 @@
+'use client';
+import { useState } from "react";
 import { RandomFox } from "@/components/RandomFox";
 
 const random = () => Math.floor(Math.random() * 122) + 1;
 
 export default function Home() {
+  const [images, setImages] = useState<Array<string>>([
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+    `https://randomfox.ca/images/${random()}.jpg`,
+  ]);
+
   return (
     <div>
       <main>
-        <RandomFox image={`https://randomfox.ca/images/${random()}.jpg`} />;
+      {images.map((image, index) => (
+          <div className="p-4" key={index}>
+            <RandomFox image={image} />
+          </div>
+        ))}
       </main>
     </div>
   );
