bb0b6e9b2fba5f7d5111d19dfb7bda5f5fefc23b
marco-soria <marco.soriaa7@gmail.com>
2024-08-10 16:45:38 -0500

    first component

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
-
+import { RandomFox } from "@/components/RandomFox";
 
 export default function Home() {
   return (
     <div>
       <main>
-          <h1 className="text-3xl font-bold underline">Hello World
-            
-          </h1>
-        </main>
+        <RandomFox />;
+      </main>
     </div>
   );
 }
---
+++ components/RandomFox.tsx
@@ -0,0 +1,14 @@
+const randomNumber = () => Math.floor(Math.random() * 122) + 1;
+
+export function RandomFox(): JSX.Element {
+  const image = `https://randomfox.ca/images/${randomNumber()}.jpg`;
+
+  return (
+    <img
+      width="320"
+      height="auto"
+      src={image}
+      className="mx-auto rounded-md bg-gray-300"
+    />
+  );
+}
